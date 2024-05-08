e8ed3a9cf5f573514cc57b94adcee6f8cc1053d9
Viktor Tsvigo <11_70@bk.ru>
2024-05-08 20:20:13 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -13,9 +13,7 @@ using namespace std;
 /// \brief list_of_synapses
 /// переменные:
 std::vector<unsigned long long> list_of_synapses ;
-std::vector<unsigned long long
-            //  int
-            > list_of_neurons[201] ;//={};
+std::vector<unsigned long long> list_of_neurons[201] ;//={};
 long long variable_error;
 QString
 // std::string
